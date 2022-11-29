637ae72d65d1d6dfb139d87120c7bc248a84bb7e
jeck99 <jeck99@gmail.com>
2022-11-29 11:02:05 +0200

    validation

--- validation/register.js
+++ validation/register.js
@@ -3,10 +3,16 @@ const isEmpty = require('is-empty');
 
 module.exports = validateRegister = (user) => {
     errors = {};
-    user.firstName = isEmpty(user.firstName) ? "" : user.firstName;
+    user.name = isEmpty(user.name) ? "" : user.name;
+    user.email = isEmpty(user.email) ? "" : user.email;
+    user.password = isEmpty(user.password) ? "" : user.password;
+    user.passwordConfirm = isEmpty(user.passwordConfirm) ? "" : user.passwordConfirm;
 
-    if (validator.isEmpty(user.firstName)) errors.firstName = "First Name Is required"
-    if (!validator.equals(user.password, user.confirmPassword)) errors.confirmPassword = "Passwords not match"
+    if (validator.isEmpty(user.name)) errors.name = "First Name Is required";
+    if (validator.isEmpty(user.email)) errors.email = "email Is required";
+    if(!validator.isEmail(user.email)) errors.email = "email Is not valid";
+    if (validator.isEmpty(user.password)) errors.password = "password Is required";
+    if (!validator.equals(user.password, user.passwordConfirm)) errors.passwordConfirm = "Passwords not match"
     return {
         errors,
         isValid: isEmpty(errors)
